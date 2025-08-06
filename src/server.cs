29558ab4822a35aa652b8496e4c7850baf2dc62b
Will Williams <wbw383@gmail.com>
2025-08-06 08:54:20 -0500

    comments

--- src/server.cs
+++ src/server.cs
@@ -1,15 +1,20 @@
 // server.cs
+// more comments
+
+// This file contains the server class which handles server operations.
 class Server
 {
+    // Method to start the server
     public void Start()
     {
         // Start the server
         System.Console.WriteLine("Server started.");
     }
 
+    // Method to stop the server
     public void Stop()
     {
         // Stop the server
         System.Console.WriteLine("Server stopped.");
-    }   
+    }
 }
