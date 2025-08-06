411250f5e21f9569bb0a2292d99f40d48a1728dd
Will Williams <wbw383@gmail.com>
2025-08-06 08:08:36 -0500

    stop server

--- src/server.cs
+++ src/server.cs
@@ -4,5 +4,12 @@ class Server
     public void Start()
     {
         // Start the server
+        System.Console.WriteLine("Server started.");
     }
+
+    public void Stop()
+    {
+        // Stop the server
+        System.Console.WriteLine("Server stopped.");
+    }   
 }
